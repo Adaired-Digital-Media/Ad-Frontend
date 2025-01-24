2ab26250f64c0c0e6689ac9606fc9aebe973ac77
Kumar <bittu.kumar@adaired61.com>
2025-01-24 11:34:38 +0530

    New Schema added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,20 @@
 import { Toaster } from 'react-hot-toast';
-import { cn } from '../@core/utils/class-names';
+import { cn } from '@core/utils/class-names';
 import { inter, lexendDeca, nunito, dm, baby, poppins } from '@/app/fonts';
 import Script from 'next/script';
 import { siteConfig } from '@/config/site.config';
 import NextProgress from '@core/components/next-progress';
-// import { ReCaptchaProvider } from 'next-recaptcha-v3';
-// Dynamically import ReCaptchaProvider
 const LazyReCaptchaProvider = dynamic(
-  () =>
-    import('next-recaptcha-v3').then((mod) => mod.ReCaptchaProvider),
-  { ssr: false } // Ensures it is only loaded on the client side
+  () => import('next-recaptcha-v3').then((mod) => mod.ReCaptchaProvider),
+  { ssr: true }
 );
 
 // styles
 import '@/app/globals.css';
-// import GlobalDrawer from '@/app/shared/drawer-views/container';
-// import GlobalModal from '@/app/shared/modal-views/container';
 import dynamic from 'next/dynamic';
-const GlobalDrawer = dynamic(() => import('@/app/shared/drawer-views/container'));
+const GlobalDrawer = dynamic(
+  () => import('@/app/shared/drawer-views/container')
+);
 const GlobalModal = dynamic(() => import('@/app/shared/modal-views/container'));
 
 export const metadata = {
@@ -39,7 +36,8 @@ const schemaData = {
     '@context': 'https://schema.org',
     '@type': 'ProfessionalService',
     name: 'AdAired Digital Media',
-    image: 'https://www.adaired.com/_next/static/media/Logo.441e7f4f.svg',
+    image:
+      'https://res.cloudinary.com/adaired/image/upload/c_limit,w_1920/f_auto/q_auto/v1/Static%20Website%20Images/adaired_logo.png?_a=BAVAZGDW0',
     url: 'https://www.adaired.com/',
     telephone: '8907400008',
     address: {
@@ -74,7 +72,7 @@ const schemaData = {
     '@type': 'Organization',
     name: 'AdAired Digital Media',
     url: 'https://www.adaired.com/',
-    logo: 'https://www.adaired.com/_next/static/media/Logo.441e7f4f.svg',
+    logo: 'https://res.cloudinary.com/adaired/image/upload/c_limit,w_1920/f_auto/q_auto/v1/Static%20Website%20Images/adaired_logo.png?_a=BAVAZGDW0',
     contactPoint: {
       '@type': 'ContactPoint',
       telephone: '8907400008',
@@ -88,6 +86,41 @@ const schemaData = {
       'https://www.instagram.com/adaired.digital/',
       'https://in.linkedin.com/company/adaired',
       'https://www.adaired.com/',
+    ],
+  },
+  localBusiness: {
+    '@context': 'https://schema.org',
+    '@type': 'LocalBusiness',
+    name: 'Adaired Digital Media',
+    image:
+      'https://res.cloudinary.com/adaired/image/upload/c_limit,w_1920/f_auto/q_auto/v1/Static%20Website%20Images/adaired_logo.png?_a=BAVAZGDW0',
+    '@id': '',
+    url: 'https://www.adaired.com/',
+    telephone: '089074 00008',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress:
+        '5th Floor, Bestech Business Tower, B-509, Parkview Residence Colony, Sector 66',
+      addressLocality: 'Mohali',
+      postalCode: '160066',
+      addressCountry: 'IN',
+    },
+    geo: {
+      '@type': 'GeoCoordinates',
+      latitude: 30.6756426,
+      longitude: 76.7402769,
+    },
+    openingHoursSpecification: {
+      '@type': 'OpeningHoursSpecification',
+      dayOfWeek: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+      opens: '09:30',
+      closes: '19:00',
+    },
+    sameAs: [
+      'https://www.facebook.com/adaired.digital',
+      'https://www.instagram.com/adaired.digital/',
+      'https://twitter.com/adaireddigital',
+      'https://www.linkedin.com/company/adaired/',
     ],
   },
 };
@@ -115,16 +148,23 @@ export default async function RootLayout({
         <Script
           type="application/ld+json"
           id="local-schema"
-          strategy="lazyOnload"
+          strategy="beforeInteractive"
         >
           {JSON.stringify(schemaData.professionalService)}
         </Script>
         <Script
           type="application/ld+json"
           id="organization-schema"
-          strategy="lazyOnload"
+          strategy="beforeInteractive"
         >
           {JSON.stringify(schemaData.organization)}
+        </Script>
+        <Script
+          type="application/ld+json"
+          id="localBusiness-schema"
+          strategy="beforeInteractive"
+        >
+          {JSON.stringify(schemaData.localBusiness)}
         </Script>
       </head>
       <body
@@ -136,10 +176,12 @@ export default async function RootLayout({
           dm.variable,
           baby.variable,
           poppins.variable,
-          `font-inter antialiased`
+          `font-nunito antialiased`
         )}
       >
-        <LazyReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY}>
+        <LazyReCaptchaProvider
+          reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY}
+        >
           <NextProgress />
           {children}
           <Toaster />
