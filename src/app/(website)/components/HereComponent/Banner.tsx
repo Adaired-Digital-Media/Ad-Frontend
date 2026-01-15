a115b016c0933f9eaa2acdc96fb183b6961a0b62
anujaraired <anuj@adaired.org>
2026-01-15 12:58:11 +0530

    updated google partner

--- src/app/(website)/components/HereComponent/Banner.tsx
+++ src/app/(website)/components/HereComponent/Banner.tsx
@@ -104,7 +104,7 @@ const Banner = () => {
   };
 
   return (
-    <div className="animate-pulse 1366:h-[135vh] 1470:h-[100vh] 1680:h-[100vh] 1710:h-[85vh] 3xl:h-[100vh] 1600:h-[118vh] 1400:h-[115vh] relative h-[100vh] w-full pt-[10vh] md:h-[130vh] lg:h-[100vh]">
+    <div className="animate-pulse 1710:h-[85vh] relative h-[100vh] w-full pt-[10vh] md:h-[130vh] lg:h-[100vh] 1366:h-[135vh] 1400:h-[115vh] 1470:h-[100vh] 1600:h-[118vh] 1680:h-[100vh] 3xl:h-[100vh]">
       <Image
         src={hero_banner}
         fill
@@ -163,7 +163,7 @@ const Banner = () => {
           </div>
           <div className="flex gap-3">
             <a
-              href="https://share.google/3KznrgKhabO459Arc"
+              href="https://www.google.com/partners/agency?id=7775339798"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -219,7 +219,7 @@ const Banner = () => {
                   width={599}
                   height={483}
                   alt=""
-                  className={`1366:w-[28rem] 1366:h-[23rem] 1400:w-[30rem] 1400:h-[25rem] 1600:w-[32rem] 1600:h-[26rem] 1680:w-[35rem] h-[10rem] w-[15rem] transition-opacity transition-transform duration-700 ease-in-out md:h-[20rem] md:w-[32rem] lg:h-[18rem] lg:w-[24rem] xl:h-[30.188rem] xl:w-[37.438rem] 3xl:h-[30.188rem] 3xl:w-[37.438rem] ${
+                  className={`h-[10rem] w-[15rem] transition-opacity transition-transform duration-700 ease-in-out md:h-[20rem] md:w-[32rem] lg:h-[18rem] lg:w-[24rem] xl:h-[30.188rem] xl:w-[37.438rem] 1366:h-[23rem] 1366:w-[28rem] 1400:h-[25rem] 1400:w-[30rem] 1600:h-[26rem] 1600:w-[32rem] 1680:w-[35rem] 3xl:h-[30.188rem] 3xl:w-[37.438rem] ${
                     active === 'img1'
                       ? 'z-40 opacity-100'
                       : active
@@ -268,9 +268,9 @@ const Banner = () => {
               width={302}
               height={186}
               alt=""
-              className={`1366:w-[14rem] 1366:h-[8rem] 1400:w-[16rem] 1400:h-[9rem] absolute bottom-0 left-[0rem] h-[6rem] w-[8rem] transition-opacity transition-transform duration-700 ease-in-out md:bottom-[-1.5rem] md:left-[0rem] md:h-[10rem] md:w-[14rem] lg:bottom-0 xl:bottom-[0rem] xl:h-[11.625rem] xl:w-[18.875rem] 3xl:h-[11.625rem] 3xl:w-[18.875rem] ${
+              className={`absolute bottom-0 left-[0rem] h-[6rem] w-[8rem] transition-opacity transition-transform duration-700 ease-in-out md:bottom-[-1.5rem] md:left-[0rem] md:h-[10rem] md:w-[14rem] lg:bottom-0 xl:bottom-[0rem] xl:h-[11.625rem] xl:w-[18.875rem] 1366:h-[8rem] 1366:w-[14rem] 1400:h-[9rem] 1400:w-[16rem] 3xl:h-[11.625rem] 3xl:w-[18.875rem] ${
                 active === 'img3'
-                  ? '1366:translate-y-[-150%] 1400:translate-x-[70%] 1400:translate-y-[-130%] 1600:translate-x-[80%] 1680:translate-x-[90%] z-30 translate-x-[100%] translate-y-[-50%] scale-105 opacity-100 backdrop-blur-md md:translate-x-[105%] md:translate-y-[-80%] lg:translate-x-[50%] lg:translate-y-[-80%] xl:translate-x-[80%] xl:translate-y-[-110%] 3xl:translate-x-[85%] 3xl:translate-y-[-120%]'
+                  ? 'z-30 translate-x-[100%] translate-y-[-50%] scale-105 opacity-100 backdrop-blur-md md:translate-x-[105%] md:translate-y-[-80%] lg:translate-x-[50%] lg:translate-y-[-80%] xl:translate-x-[80%] xl:translate-y-[-110%] 1366:translate-y-[-150%] 1400:translate-x-[70%] 1400:translate-y-[-130%] 1600:translate-x-[80%] 1680:translate-x-[90%] 3xl:translate-x-[85%] 3xl:translate-y-[-120%]'
                   : active
                     ? 'z-10 translate-x-0 translate-y-0 scale-100 opacity-40'
                     : 'z-10 translate-x-0 translate-y-0 scale-100 opacity-100'
@@ -281,9 +281,9 @@ const Banner = () => {
               width={388}
               height={244}
               alt=""
-              className={`1366:w-[18rem] 1366:h-[10rem] 1366:top-[8rem] 1400:w-[20rem] 1400:top-[5rem] 1400:h-[12rem] 1680:top-[1.5rem] absolute right-0 top-[-4rem] h-[6.875rem] w-[10.563rem] transition-opacity transition-transform duration-700 ease-in-out md:top-[-2rem] md:h-[13rem] md:w-[20rem] lg:top-[30%] lg:h-[10.25rem] lg:w-[15.25rem] xl:top-[0rem] xl:h-[15.25rem] xl:w-[24.25rem] 3xl:h-[15.25rem] 3xl:w-[24.25rem] ${
+              className={`absolute right-0 top-[-4rem] h-[6.875rem] w-[10.563rem] transition-opacity transition-transform duration-700 ease-in-out md:top-[-2rem] md:h-[13rem] md:w-[20rem] lg:top-[30%] lg:h-[10.25rem] lg:w-[15.25rem] xl:top-[0rem] xl:h-[15.25rem] xl:w-[24.25rem] 1366:top-[8rem] 1366:h-[10rem] 1366:w-[18rem] 1400:top-[5rem] 1400:h-[12rem] 1400:w-[20rem] 1680:top-[1.5rem] 3xl:h-[15.25rem] 3xl:w-[24.25rem] ${
                 active === 'img2'
-                  ? '1400:translate-x-[-40%] 1600:translate-x-[-50%] 1680:translate-x-[-60%] 1680:translate-y-[110%] z-30 translate-x-[-50%] translate-y-[50%] scale-105 bg-white/80 opacity-100 backdrop-blur-md md:translate-x-[-60%] md:translate-y-[75%] lg:translate-x-[-45%] lg:translate-y-[80%] xl:translate-x-[-50%] xl:translate-y-[100%] 3xl:translate-x-[-49%] 3xl:translate-y-[90%]'
+                  ? 'z-30 translate-x-[-50%] translate-y-[50%] scale-105 bg-white/80 opacity-100 backdrop-blur-md md:translate-x-[-60%] md:translate-y-[75%] lg:translate-x-[-45%] lg:translate-y-[80%] xl:translate-x-[-50%] xl:translate-y-[100%] 1400:translate-x-[-40%] 1600:translate-x-[-50%] 1680:translate-x-[-60%] 1680:translate-y-[110%] 3xl:translate-x-[-49%] 3xl:translate-y-[90%]'
                   : active
                     ? 'z-10 translate-x-0 translate-y-0 scale-100 opacity-40'
                     : 'z-10 translate-x-0 translate-y-0 scale-100 opacity-100'
